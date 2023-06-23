6236d93590241865cb2a8610c8c4c1e3cdc13299
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-06-23 10:46:35 +0700

    include iostream

--- CircularQuenues_172/CircularQuenues_172.cpp
+++ CircularQuenues_172/CircularQuenues_172.cpp
@@ -1,20 +1,63 @@
-// CircularQuenues_172.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	void insert() {
+		int num;
+		cout << " Enter a number : ";
+		cin >> num;
+		cout << endl;
+
+		// cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueues overflow\n";
+			return;
+		}
+
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+		// cek apakah antrian kosong 
+		if (FRONT == -1) {
+			cout << " Queue underflow\n ";
+			return;
+		}
+		cout << "\nThe element delected from the queue is : " << queue_array[FRONT] << "\n";
+
+		// cek jika antrian hanya memiliki satu element
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
+};
